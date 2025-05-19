6b54e62774f1e636ad9be3deb1a704c9006a3c5f
Starshix <d_vybornov06@mail.ru>
2025-05-19 19:51:20 +0300

    laba6

---
+++ dev env/app/goobs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from goobs import views
+
+
+app_name = "goobs"
+
+urlpatterns = [
+    path('', views.catalog, name='catalog'),
+    path('product/', views.product, name='product'),
+]
